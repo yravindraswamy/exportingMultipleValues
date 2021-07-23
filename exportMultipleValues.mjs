121acf748cc52575178af38fea3c94e1b6b862ee
yravindraswamy <yravindraswamy@gmail.com>
2021-07-23 14:44:43 +0000

    one line is added in export file

---
+++ exportMultipleValues.mjs
@@ -0,0 +1,6 @@
+const myArray = ["camel", 265, true, "5.6"];
+const bulb = { watts: 10, type: "LED" };
+function multiplyByFour(number) {
+  return number * 4;
+}
+export { myArray, bulb, multiplyByFour };
